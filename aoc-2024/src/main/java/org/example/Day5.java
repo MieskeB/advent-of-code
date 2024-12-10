fbf0c8707be88854eeeda3b1abf4ae4fb714a4dc
Michel Bijnen <info@michelbijnen.nl>
2024-12-10 15:56:40 +0100

    fix: gave day 5 another attempt

--- aoc-2024/src/main/java/org/example/Day5.java
+++ aoc-2024/src/main/java/org/example/Day5.java
@@ -26,60 +26,84 @@ public class Day5 {
             }
         }
 
-        List<Book> incorrectBooks = new ArrayList<>();
+
         int correctCountedResults = 0;
         for (Book book : books) {
-            boolean incorrectBook = false;
-            boolean incorrectBookIsAdded = false;
-            for (Rule rule : rules) {
-                boolean foundAfter = false;
-                for (String page : book) {
-                    if (page.equals(rule.after))
-                        foundAfter = true;
-                    if (page.equals(rule.before)) {
-                        if (foundAfter) {
-                            book.addCollidingRule(rule);
-                            if (!incorrectBookIsAdded) {
-                                incorrectBookIsAdded = true;
-                                incorrectBooks.add(book);
-                                incorrectBook = true;
-                            }
-                            break;
-                        }
-                        break;
-                    }
-                }
-            }
-
-            if (!incorrectBook)
+            if (isValid(book, rules))
                 correctCountedResults += book.getMedian();
         }
         System.out.println("Correct median count: " + correctCountedResults);
 
 
+        List<String> orderedList = getOrder(rules);
         int correctedCountedResults = 0;
-        for (Book book : incorrectBooks) {
-            Collections.reverse(book.collidingRules);
-            for (Rule rule : book.collidingRules) {
-                int is = 0;
-                int ie = 0;
-                for (int i = 0; i < book.getPages().size(); i++) {
-                    if (book.getPages().get(i).equals(rule.before)) {
-                        is = i;
-                    } else if (book.getPages().get(i).equals(rule.after)) {
-                        ie = i;
-                    }
-                }
-                String ts = book.getPages().get(is);
-                String te = book.getPages().get(ie);
-                book.getPages().set(is, te);
-                book.getPages().set(ie, ts);
+        for (Book book : books) {
+            if (isValid(book, rules)) continue;
+
+            Map<String, Integer> orderMap = new HashMap<>();
+            for (int i = 0; i < orderedList.size(); i++) {
+                orderMap.put(orderedList.get(i), i);
             }
+
+            book.pages.sort((a, b) -> {
+                int indexA = orderMap.getOrDefault(a, Integer.MAX_VALUE);
+                int indexB = orderMap.getOrDefault(b, Integer.MAX_VALUE);
+                return Integer.compare(indexA, indexB);
+            });
 
             correctedCountedResults += book.getMedian();
         }
 
         System.out.println("Corrected median count: " + correctedCountedResults);
+    }
+
+
+    private static boolean isValid(Book book, List<Rule> ruleSet) {
+        for (Rule r : ruleSet) {
+            boolean afterFound = false;
+            for (String page : book.getPages()) {
+                if (page.equals(r.before)) {
+                    if (afterFound) {
+                        return false;
+                    }
+                } else if (page.equals(r.after)) {
+                    afterFound = true;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static List<String> getOrder(List<Rule> ruleSet) {
+        List<String> orderedList = new ArrayList<>();
+
+        for (Rule rule : ruleSet) {
+            if (!orderedList.contains(rule.before)) {
+                orderedList.add(rule.before);
+            }
+
+            int beforeIndex = orderedList.indexOf(rule.before);
+            if (!orderedList.contains(rule.after)) {
+                if (beforeIndex == orderedList.size() - 1) {
+                    orderedList.add(rule.after);
+                } else {
+                    orderedList.add(orderedList.indexOf(rule.before) + 1, rule.after);
+                }
+            } else {
+                int afterIndex = orderedList.indexOf(rule.after);
+                if (afterIndex < beforeIndex) {
+                    orderedList.remove(afterIndex);
+                    beforeIndex--;
+                    if (beforeIndex == orderedList.size() - 1) {
+                        orderedList.add(rule.after);
+                    } else {
+                        orderedList.add(beforeIndex + 1, rule.after);
+                    }
+                }
+            }
+        }
+
+        return orderedList;
     }
 
 
@@ -99,16 +123,10 @@ public class Day5 {
     @Data
     private static class Book implements Iterable<String> {
         private List<String> pages;
-        private List<Rule> collidingRules;
 
         public Book(String inp) {
-            this.collidingRules = new ArrayList<>();
             this.pages = new ArrayList<>();
             this.pages.addAll(Arrays.asList(inp.split(",")));
-        }
-
-        public void addCollidingRule(Rule rule) {
-            this.collidingRules.add(rule);
         }
 
         public int getMedian() {
