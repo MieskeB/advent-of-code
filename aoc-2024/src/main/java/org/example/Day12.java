0e0abc4ad33e9370a857f1af0f8e0d1b10e68a10
Michel Bijnen <info@michelbijnen.nl>
2024-12-13 14:40:18 +0100

    feat: added day12 and day13

---
+++ aoc-2024/src/main/java/org/example/Day12.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.List;
+
+public class Day12 {
+    public static void main(String[] args) {
+        List<String> fileContent = Utils.getContentFromFileByLine(12, "input.txt");
+    }
+}
