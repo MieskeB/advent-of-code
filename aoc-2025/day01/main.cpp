74f7a40cf00473cdb9f6b66fe0cb0238bfe958db
Michel Bijnen <info@michelbijnen.nl>
2025-12-01 12:59:33 +0100

    feat: added challenge 1.1

--- aoc-2025/day01/main.cpp
+++ aoc-2025/day01/main.cpp
@@ -20,11 +20,34 @@ int main() {
     }
 
     std::vector<std::string> lines;
-    for (std::string line; getline(infile, line); ) {
+    for (std::string line; getline(infile, line);) {
         lines.push_back(line);
     }
 
-    for (const std::string& line : lines) {
-        std::cout << line << std::endl;
+    int dial = 50;
+    int count = 0;
+    for (const std::string &line: lines) {
+        const char dir = line[0];
+        int amount = 0;
+        if (line.length() == 2) {
+            amount = line[1] - '0';
+        } else if (line.length() == 3) {
+            amount = (line[1] - '0') * 10 + (line[2] - '0');
+        } else {
+            amount = (line[1] - '0') * 100 + (line[2] - '0') * 10 + (line[3] - '0');
+        }
+        if (dir == 'L') {
+            dial = (dial - amount) % 100;
+        } else {
+            dial = (dial + amount) % 100;
+        }
+        if (dial < 0) {
+            dial = 100 + dial;
+        }
+        if (dial == 0) {
+            count++;
+        }
     }
+
+    std::cout << count << std::endl;
 }
