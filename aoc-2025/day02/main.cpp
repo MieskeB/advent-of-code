065cecebd32cf706034762301e156fd43007e311
Michel Bijnen <info@michelbijnen.nl>
2025-12-02 09:52:41 +0100

    feat: added day2

--- aoc-2025/day02/main.cpp
+++ aoc-2025/day02/main.cpp
@@ -10,6 +10,20 @@ std::filesystem::path get_input_path() {
     return input_path;
 }
 
+std::vector<std::string> split(const std::string &s, const std::string &delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        std::string token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
 int main() {
     std::filesystem::path input_path = get_input_path();
     std::ifstream infile(input_path);
@@ -20,11 +34,50 @@ int main() {
     }
 
     std::vector<std::string> lines;
-    for (std::string line; getline(infile, line); ) {
+    for (std::string line; getline(infile, line);) {
         lines.push_back(line);
     }
 
-    for (const std::string& line : lines) {
-        std::cout << line << std::endl;
+    std::string line = lines[0];
+    std::vector<std::string> words = split(line, ",");
+
+    long long count_a = 0;
+    long long count_b = 0;
+    for (std::string w: words) {
+        std::vector<std::string> splitted = split(w, "-");
+        long long w0 = std::stoll(splitted[0]);
+        long long w1 = std::stoll(splitted[1]);
+        for (long long i = w0; i <= w1; i++) {
+            std::string word = std::to_string(i);
+            if (word.length() % 2 == 1) {
+                continue;
+            }
+            if (int half = word.length() / 2; word.substr(0, half) == word.substr(half, half)) {
+                count_a += i;
+            }
+        }
+        for (long long i = w0; i <= w1; i++) {
+            std::string word = std::to_string(i);
+            int half = word.length() / 2;
+            for (int j = 1; j <= half; j++) {
+                if (word.length() % j != 0) {
+                    continue;
+                }
+                std::string to_check = word.substr(0, j);
+                bool all_same = true;
+                for (int k = j; k < word.length(); k += j) {
+                    if (word.substr(k, j) != to_check) {
+                        all_same = false;
+                        break;
+                    }
+                }
+                if (all_same) {
+                    count_b += i;
+                    break;
+                }
+            }
+        }
     }
+    std::cout << count_a << std::endl;
+    std::cout << count_b << std::endl;
 }
