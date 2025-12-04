da2055615d37ea822e0db9517436df58b995f465
Michel Bijnen <info@michelbijnen.nl>
2025-12-04 23:35:29 +0100

    feat: added day 4

--- aoc-2025/day04/main.cpp
+++ aoc-2025/day04/main.cpp
@@ -10,6 +10,45 @@ std::filesystem::path get_input_path() {
     return input_path;
 }
 
+std::vector<std::tuple<int, int>> directions_mult {
+    {0, -1},
+    {1, -1},
+    {1, 0},
+    {1, 1},
+    {0, 1},
+    {-1, 1},
+    {-1, 0},
+    {-1, -1}
+};
+
+std::vector<std::tuple<int, int>> get_positions(std::vector<std::string> lines) {
+    std::vector<std::tuple<int, int>> positions;
+    int x_bound = lines[0].size();
+    int y_bound = lines.size();
+    for (int y = 0; y < lines.size(); y++) {
+        for (int x = 0; x < lines[0].size(); x++) {
+            if (lines[y][x] != '@') {
+                continue;
+            }
+
+            int count = 0;
+            for (auto [dx, dy] : directions_mult) {
+                if (x + dx < 0 || y + dy < 0 || x + dx >= x_bound || y + dy >= y_bound) {
+                    continue;
+                }
+                if (lines[y + dy][x + dx] == '@') {
+                    count++;
+                }
+            }
+
+            if (count < 4) {
+                positions.emplace_back(x, y);
+            }
+        }
+    }
+    return positions;
+}
+
 int main() {
     std::filesystem::path input_path = get_input_path();
     std::ifstream infile(input_path);
@@ -24,7 +63,17 @@ int main() {
         lines.push_back(line);
     }
 
-    for (const std::string& line : lines) {
-        std::cout << line << std::endl;
+    std::vector<std::tuple<int, int>> positions = get_positions(lines);
+    std::cout << "res a: " << positions.size() << std::endl;
+
+    int count = 0;
+    while (positions.size() != 0) {
+        count += positions.size();
+        for (auto [x, y] : positions) {
+            lines[y][x] = 'x';
+        }
+        positions = get_positions(lines);
     }
+
+    std::cout << "res b: " << count << std::endl;
 }
