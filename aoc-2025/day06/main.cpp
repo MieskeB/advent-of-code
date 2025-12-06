b6671eab7de7c4f8ddbfa1152a0ba56111a06806
Michel Bijnen <info@michelbijnen.nl>
2025-12-06 21:55:01 +0100

    feat: added day 6

--- aoc-2025/day06/main.cpp
+++ aoc-2025/day06/main.cpp
@@ -2,12 +2,24 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cctype>
 
 std::filesystem::path get_input_path() {
     std::filesystem::path source_path = std::filesystem::path(__FILE__);
     std::filesystem::path dir = source_path.parent_path();
     std::filesystem::path input_path = dir / "input.txt";
     return input_path;
+}
+
+int find_length_longest_word(const std::vector<std::string> &words) {
+    int longest_word = 0;
+    for (std::string word : words) {
+        if (word.length() > longest_word) {
+            longest_word = word.length();
+        }
+    }
+    return longest_word;
 }
 
 int main() {
@@ -24,7 +36,68 @@ int main() {
         lines.push_back(line);
     }
 
-    for (const std::string& line : lines) {
-        std::cout << line << std::endl;
+
+    long long res_a = 0;
+    for (int i = 0; i < lines.back().size(); i++) {
+        if (lines.back()[i] == ' ') {
+            continue;
+        }
+        long long temp_res = 1;
+        for (int j = 0; j < lines.size() - 1; j++) {
+            std::string number_word = "";
+            bool has_started = false;
+            for (int k = i; k < i + 20; k++) {
+                auto digit = lines[j][k];
+                if (!has_started && digit == ' ') {
+                    continue;
+                } else if (has_started && digit == ' ') {
+                    break;
+                }
+                has_started = true;
+                number_word += digit;
+            }
+            if (lines.back()[i] == '*') {
+                temp_res *= std::stoll(number_word);
+            } else if (lines.back()[i] == '+') {
+                res_a += std::stoll(number_word);
+            }
+        }
+        if (lines.back()[i] == '*') {
+            res_a += temp_res;
+        }
     }
+
+    std::cout << "Res a: " << res_a << std::endl;
+
+    long long res_b;
+    int line_length = find_length_longest_word(lines);
+    std::vector<long long> numbers;
+    for (int i = line_length - 1; i >= 0; i--) {
+        std::string number_word = "";
+        for (int j = 0; j < lines.size() - 1; j++) {
+            if (lines[j].length() > i) {
+                number_word += lines[j][i];
+            }
+        }
+        numbers.push_back(std::stoll(number_word));
+        if (lines.back().length() > i) {
+            if (lines.back()[i] == '*') {
+                long long temp_res = 1;
+                for (long long number : numbers) {
+                    temp_res *= number;
+                }
+                res_b += temp_res;
+                numbers.clear();
+                i--;
+            } else if (lines.back()[i] == '+') {
+                for (long long number : numbers) {
+                    res_b += number;
+                }
+                numbers.clear();
+                i--;
+            }
+        }
+    }
+
+    std::cout << "Res b: " << res_b << std::endl;
 }
