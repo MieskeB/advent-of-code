5a79333109454b8e6566934424d0a49f413f880c
Michel Bijnen <info@michelbijnen.nl>
2025-12-10 14:23:41 +0100

    feat: added day 10 part 1

--- aoc-2025/day10/main.cpp
+++ aoc-2025/day10/main.cpp
@@ -1,6 +1,9 @@
+#include <any>
+#include <array>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <ranges>
 #include <vector>
 
 std::filesystem::path get_input_path() {
@@ -8,6 +11,68 @@ std::filesystem::path get_input_path() {
     std::filesystem::path dir = source_path.parent_path();
     std::filesystem::path input_path = dir / "input.txt";
     return input_path;
+}
+
+std::vector<std::string> split(const std::string &s, const std::string &delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        std::string token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
+bool is_valid(std::vector<int> goal, std::vector<bool> states) {
+    for (int g : goal) {
+        if (!states[g]) {
+            return false;
+        }
+        states[g] = false;
+    }
+    for (const bool state : states) {
+        if (state) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool try_sequences(int depth, int max_depth, const std::vector<std::vector<int>> &buttons, std::vector<bool> &states, const std::vector<int> &goal) {
+    if (depth == max_depth) {
+        return is_valid(goal, states);
+    }
+
+    for (std::size_t i = 0; i < buttons.size(); i++) {
+        for (int action : buttons[i]) {
+            states[action] = !states[action];
+        }
+
+        if (try_sequences(depth + 1, max_depth, buttons, states, goal)) {
+            return true;
+        }
+
+        for (int action : buttons[i]) {
+            states[action] = !states[action];
+        }
+    }
+    return false;
+}
+
+int fewest_presses(std::vector<int> goal, int goal_length, std::vector<std::vector<int>> buttons) {
+    std::vector<bool> states(goal_length, false);
+    for (int presses = 1; presses < 20; presses++) {
+        std::vector<bool> tmp = states;
+
+        if (try_sequences(0, presses, buttons, tmp, goal)) {
+            return presses;
+        }
+    }
+    return -1;
 }
 
 int main() {
@@ -24,7 +89,35 @@ int main() {
         lines.push_back(line);
     }
 
+    int res = 0;
     for (const std::string& line : lines) {
-        std::cout << line << std::endl;
+        std::vector<std::string> buttons_s = split(line, " ");
+        std::string goal_s = buttons_s[0];
+        std::string idk_s = buttons_s.back();
+        buttons_s.erase(buttons_s.begin());
+        buttons_s.pop_back();
+
+        int goal_length = goal_s.length() - 2;
+        std::vector<int> goal;
+        for (int i = 1; i < goal_length + 1; i++) {
+            if (goal_s[i] == '#') {
+                goal.push_back(i - 1);
+            }
+        }
+
+        std::vector<std::vector<int>> buttons;
+        for (std::string &button_s : buttons_s) {
+            button_s = button_s.substr(1, button_s.size() - 2);
+            std::vector<std::string> splitted = split(button_s, ",");
+            std::vector<int> actions;
+            for (const std::string& action_s : splitted) {
+                actions.push_back(std::stoi(action_s));
+            }
+            buttons.push_back(actions);
+        }
+
+        res += fewest_presses(goal, goal_length, buttons);
     }
+
+    std::cout << res << std::endl;
 }
